4d6bd6e80354ccdfb3bd42e3292dd32a955de1b3
Aleksandra Jurkiewicz <aleksandra.jurkiewicz2003@gmail.com>
2025-08-15 22:12:27 +0200

    background color added

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,8 @@ export const theme = {
     color: {
         black: "#000000",
         white: "#FFFFFF",
-        grey: "#7E839A"
+        grey: "#7E839A",
+        lightGrey: "#F5F5FA",
     },
 
     breakpoint: {
