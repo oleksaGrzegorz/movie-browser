d3ca86285e70c35d6b03f5b795ce026f32a48d0f
Grzegorz Oleksa <grzesieq92@gmail.com>
2025-08-30 14:09:35 +0200

    added provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { ThemeProvider } from 'styled-components'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+import App from "./App.jsx";
+import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import { GlobalStyle } from './GlobalStyle.js'
+import { GlobalStyle } from "./GlobalStyle.js";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </StrictMode>
+);
